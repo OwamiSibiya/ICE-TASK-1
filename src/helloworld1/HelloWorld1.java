6edcc23c0edc86f6c8866890fa6edf5386eff091
OwamiSibiya <105348692+OwamiSibiya@users.noreply.github.com>
2023-04-25 12:14:14 +0000

    Add files via upload

---
+++ src/helloworld1/HelloWorld1.java
@@ -0,0 +1,11 @@
+package helloworld1;
+import java.util.Scanner;
+
+public class HelloWorld1 {
+
+    
+    public static void main(String[] args) {
+       System.out.println("Hello, World");
+    }
+    
+}
